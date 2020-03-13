75dba76035b65c768b3f6ab1079bf9e1e6a591b7
chachae <chenyuexin1998@gmail.com>
2020-03-13 11:21:53 +0800

    feat: 完成所有模块的无刷新升级

---
+++ oes-core/src/main/java/com/chachae/exam/rest/CaptchaController.java
@@ -0,0 +1,12 @@
+package com.chachae.exam.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 验证码 rest 控制器
+ *
+ * @author chachae
+ * @since 2020/3/13 11:20
+ */
+@RestController("/api/captcha")
+public class CaptchaController {}
